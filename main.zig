c962ad731ebb9b8bc71714dd78e4515d90fc506e
diqye <diqye@diqyes-MacBook-Air.local>
2025-06-30 17:35:42 +0800

    support backup data

--- main.zig
+++ main.zig
@@ -110,11 +110,7 @@ const Entry = struct {
 
 };
 
-test "only one" {
-    const act = std.testing.allocator;
-    _ = act;
-    std.debug.print("{}", .{0xff ^ 0x88});
-}
+
 fn errHanding(err: argsParser.Error) anyerror!void {
     std.debug.print("{}",.{err});
 }
@@ -127,10 +123,94 @@ fn print(executable_name: ?[:0]const u8) void {
     \\ --delete [-d]        删除key和--key同时使用
     \\ --list   [-l]        列出所有的key
     \\ --help   [-h]        打印帮助信息
+    \\ --push               将数据文件推送到gitee上,需要环境变量
+    \\                      $gitee_clipboard_token=私有令牌
+    \\                      $gitee_store_path=https://gitee.com/api/v5/repos/diqye/store/contents/{{path}}
+    \\                      其中 {{path}} 为占位符，程序会自动生成名字替换它。
     \\
     ;
     std.debug.print(help, .{executable_name});
     std.process.exit(0);
+}
+fn getDays() ![9]u8 {
+    // now secends 
+    const now : u32 = @intCast(std.time.timestamp() - 1751263280);
+    const days : u32 = @divTrunc(now, std.time.s_per_day);
+    var result : [9]u8 = undefined;
+    _ = try std.fmt.bufPrint(&result, "{d:0>5}days", .{days});
+    return result;
+
+}
+test getDays{
+    const json_str = \\ {"content":{"name":"00000daysago.b","path":"00000daysago.b","size":123,"sha":"3f1b2fb499b1ed5b3f94b0ba1abea6f2c50966fd","type":"file","url":"https://gitee.com/api/v5/repos/diqye/store/contents/00000daysago.b","html_url":"https://gitee.com/diqye/store/blob/master/00000daysago.b","download_url":"https://gitee.com/diqye/store/raw/master/00000daysago.b","_links":{"self":"https://gitee.com/api/v5/repos/diqye/store/contents/00000daysago.b","html":"https://gitee.com/diqye/store/blob/master/00000daysago.b"}},"commit":{"sha":"fcf4f5927ec2d30905f855a934402d5b8364b698","author":{"name":"Rezero","email":"262666212@qq.com","date":"2025-06-30T06:39:43+00:00"},"committer":{"name":"Gitee","email":"noreply@gitee.com","date":"2025-06-30T06:39:43+00:00"},"message":"commit aotocally by clipboard","tree":{"sha":"0e5b430ac05387b6611ba56d438bcceb21733812","url":"https://gitee.com/api/v5/repos/diqye/store/git/trees/0e5b430ac05387b6611ba56d438bcceb21733812"},"parents":[{"sha":"843b776f5832d735d294e8b221a5d636935b9623","url":"https://gitee.com/api/v5/repos/diqye/store/commits/843b776f5832d735d294e8b221a5d636935b9623"}]}}
+    \\
+    ;
+    const start = std.mem.indexOf(u8, json_str,"html_url\":\"").? + 11;
+    const start_str = json_str[start..];
+    const end = std.mem.indexOfScalar(u8, start_str, '"').?;
+    std.debug.print("{s}\n", .{start_str[0..end]});
+}
+/// 将文件推送到gitee
+fn push(file: std.fs.File) !void {
+    const allocator = std.heap.page_allocator;
+    // base64 编码
+    var list = std.ArrayList(u8).init(allocator);
+    defer list.deinit();
+    try std.base64.standard.Encoder.encodeFromReaderToWriter(list.writer(), file.reader());
+    // http
+    var client = std.http.Client{
+        .allocator = allocator,
+    };
+    defer client.deinit();
+    // token
+    const token = try std.process.getEnvVarOwned(allocator, "gitee_clipboard_token");
+    defer allocator.free(token);
+
+    // gitee path
+    const gitee_store_path= try std.process.getEnvVarOwned(allocator, "gitee_store_path");
+    defer allocator.free(gitee_store_path);
+
+    // path
+    const name = try getDays();
+    const path = try std.fmt.allocPrint(allocator, "{s}.b", .{&name});
+    defer allocator.free(path);
+
+    const gitee_path = try std.mem.replaceOwned(u8, allocator, gitee_store_path, "{path}", path);
+    defer allocator.free(gitee_path);
+
+    
+    // payload
+    var obj = std.json.ObjectMap.init(allocator);
+    defer obj.deinit();
+    try obj.put("access_token", .{.string = token});
+    try obj.put("content", .{.string = list.items});
+    try obj.put("message", .{.string = "commit aotocally by clipboard"});
+    const payload = try std.json.stringifyAlloc(allocator, std.json.Value{
+        .object = obj,
+    }, .{});
+    defer allocator.free(payload);
+    // resposne
+    var response = std.ArrayList(u8).init(allocator);
+    defer response.deinit();
+
+    const result = try client.fetch(.{
+        .location = .{ .url = gitee_path},
+        .method = .POST,
+        .payload = payload,
+        .headers = .{
+            .content_type = .{ .override =  "application/json" }
+        },
+        .response_storage = .{ .dynamic = &response },
+    });
+    const start = std.mem.indexOf(u8, response.items,"html_url\":\"");
+    if(start)|start_index| {
+        const start_str = response.items[start_index + 11 ..];
+        const end = std.mem.indexOfScalar(u8, start_str, '"').?;
+        std.debug.print("Success {} {s}\n", .{result.status,start_str[0..end]});
+    } else {
+        std.debug.print("Failed {} {s}\n", .{result.status,response.items});
+    }
+
 }
 pub fn main() !void {
     const allocator = std.heap.page_allocator;
@@ -143,6 +223,7 @@ pub fn main() !void {
         /// 列出所有的 key
         list: bool = false,
         help: bool = false,
+        push: bool = false,
 
         // This declares short-hand options for single hyphen
         pub const shorthands = .{
@@ -194,6 +275,10 @@ pub fn main() !void {
         try entry.writeAll();
     } else if(options.options.help) {
         print(options.executable_name);
+    } else if(options.options.push) {
+        var entry = try Entry.init(allocator);
+        defer entry.deinit();
+        try push(entry.file);        
     } else {
         print(options.executable_name);
     }
